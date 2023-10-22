f8c6a25ef5aa5a575f2a27b549ef48acbf24a066
Rembrandt <nanachi1027@163.com>
2023-10-22 13:28:05 +0800

    update standalone cluster in spark module

---
+++ modules/spark/src/test/java/containers/SparkStandaloneContainerClusterTest.java
@@ -0,0 +1,24 @@
+package containers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.SparkStandaloneContainerCluster;
+
+public class SparkStandaloneContainerClusterTest {
+    @Test
+    public void testSparkContainerCluster() throws Exception {
+        try (SparkStandaloneContainerCluster cluster =
+                 new SparkStandaloneContainerCluster(2)) {
+            cluster.start();
+
+            Assertions.assertTrue(cluster.getMaster().isRunning());
+            Assertions.assertTrue(cluster.getWorkers().size() == 2);
+            cluster.getWorkers().stream().forEach(worker -> {
+                int workerPort = worker.getMappedPort(cluster.SPARK_WORKER_PORT);
+                Assertions.assertTrue(workerPort > 0);
+            });
+        }
+    }
+
+    // try to commit a wordcount app to the cluster
+}
